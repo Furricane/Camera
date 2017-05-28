b55c8b6f798f4adfbc66c3c89b2cdb3087f96e7a
Furricane <kinkajoux@gmail.com>
2017-05-28 11:02:45 -0500

    GoogleDrive

--- on_motion_script.py
+++ on_motion_script.py
@@ -10,12 +10,26 @@ import globals
 from datetime import datetime
 from datetime import timedelta
 import gmail
+import GoogleDrive
+
+
+def listdir_shell(path, *lsargs):
+    p = Popen(('ls', path) + lsargs, shell=False, stdout=PIPE, close_fds=True)
+    #for path in p.stdout.readlines():
+    #    print(path.strip())
+    #return [path.rstrip('\n') for path in p.stdout.readlines()]
+    return [path.strip() for path in p.stdout.readlines()]
+
+dirlist = listdir_shell('/home/pi/Camera/Capture/', '-t')[:10]
 
 
 print("Starting nofify loop")
 gmail.SendMail("Camera Motion Detected","Camera Motion Detected")
 
-
+folder = GoogleDrive.CreateFolder('CameraTest')
+for capturefile in dirlist:
+    print(capturefile.decode())
+    GoogleDrive.UploadFile('/home/pi/Camera/Capture/',capturefile.decode(), folder)
 
 
 
