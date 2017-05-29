2b2d011e5522a1cb23c60bbcfcffd82da00f8d0c
Furricane <kinkajoux@gmail.com>
2017-05-29 10:30:27 -0500

    Latest

--- on_motion_script.py
+++ on_motion_script.py
@@ -25,14 +25,27 @@ def listdir_shell(path, *lsargs):
 
 dirlist = listdir_shell('/home/pi/Camera/Capture/', '-t')[:10]
 
-
 print("Starting nofify loop")
 gmail.SendMail("Camera Motion Detected","Camera Motion Detected")
 
 folder = GoogleDrive.CreateFolder('CameraTest')
+filelist, ids = GoogleDrive.GetFileList('CameraTest')
+
+filelistnopaths=[]
+for f in filelist:
+    #print ("file ",f)
+    path, filename = os.path.split(f)
+    #print("path= ",path,"filename=",filename)
+    filelistnopaths.append(filename)
+    print("filename=",filename)
+
 for capturefile in dirlist:
     print(capturefile.decode())
-    GoogleDrive.UploadFile('/home/pi/Camera/Capture/',capturefile.decode(), folder)
+    if capturefile.decode() in filelistnopaths:
+         print("Duplicate file, not uploading")
+    else:
+        GoogleDrive.UploadFile('/home/pi/Camera/Capture/',capturefile.decode(), folder)
+
 
 
 
